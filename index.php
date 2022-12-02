8a834174d7e54eede7c3f20acae364f026439dc4
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-12-02 15:59:01 +0100

    first

--- index.php
+++ index.php
@@ -1,47 +1,3 @@
-<?php
-
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,46 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- /BOOTSTRAP -->
+    <title>Generatore Password</title>
 
-    <title>Php-hotel</title>
+    <style>
+        body{
+            background-color:#e5e5e5;
+            color: #fca311;
+        }
+        h2{
+            font-size:3rem;
+        }
+        main{
+            height: 100vh;
+        }
+        .container-sm{
+            height: 50%;
+            position: absolute;
+            top:0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            margin: auto;
+            width: 65vw;
+            border-radius: 30px;
+            background-color: #14213d;
+            padding: 20px;
+        }
+    </style>
+
 </head>
-
 <body>
-
-    <!-- table -->
-
-<table class="table table-striped w-75 m-auto mt-5 text-center">
-  <thead>
-    <tr>
-      <th scope="col">Nome</th>
-      <th scope="col">Descrizione</th>
-      <th scope="col">Parcheggio</th>
-      <th scope="col">Voto</th>
-      <th scope="col">Distanza dal centro in km</th>
-    </tr>
-  </thead>
-  <tbody>
-     <?php foreach($hotels as $hotel) : ?>
-        <tr>
-            <?php foreach($hotel as $key => $value) : ?>
-                <?php if($key === 'parking' && $value) : ?>
-                    <td>
-                        <?php echo "SI" ?>
-                    </td>
-                <?php elseif($key === 'parking' && !$value) : ?>
-                    <td>
-                        <?php echo "NO" ?>
-                    </td>
-                <?php else : ?>
-                    <td>
-                        <?php echo $value ?>
-                    </td>
-                <?php endif ?>
-            <?php endforeach ?>
-         </tr>
-    <?php endforeach ?>
-  </tbody>
-
+   <main class="">
+        <div class="container-sm ">
+            <h2 class="">ciao</h2>
+        </div>
+   </main>
+    
 </body>
 </html>
